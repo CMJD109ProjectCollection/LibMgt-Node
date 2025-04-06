ad31620d42675ac83ffbc0d9b45ca0dc337fea89
ThanuraSilva <thanurasilva1989@gmail.com>
2025-04-06 12:32:57 +0530

    Create schema for Books

--- src/app.js
+++ src/app.js
@@ -5,17 +5,18 @@ const bookRoutes = require("./routes/bookRoutes")
 const staffRoutes = require("./routes/staffRoutes")
 const lendingRoutes = require("./routes/lendingRoutes")
 const memberRoutes = require("./routes/memberRoutes")
+const mongoose = require("mongoose")
 
 //handle routes
-app.use("/api/v1",bookRoutes)
+app.use("/api/v1", bookRoutes)
 // app.use("/api/v1",staffRoutes)
 // app.use("/api/v1",lendingRoutes)
 // app.use("/api/v1",memberRoutes)
 
 mongoose.connect("mongodb://localhost:27017/bookLibCMJD109",
-{useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.error("Failed to connect to MongoDB",err))
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("Failed to connect to MongoDB", err))
 
 
 app.listen(port, () => {
