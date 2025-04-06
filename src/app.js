5075ca96eb19fdd02c483143aadf2f50a9c7bb27
ThanuraSilva <thanurasilva1989@gmail.com>
2025-04-06 12:12:45 +0530

    Install Mongoose and ready for DB integrate

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ app.use("/api/v1",bookRoutes)
 // app.use("/api/v1",lendingRoutes)
 // app.use("/api/v1",memberRoutes)
 
+mongoose.connect("mongodb://localhost:27017/bookLibCMJD109",
+{useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("Failed to connect to MongoDB",err))
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
