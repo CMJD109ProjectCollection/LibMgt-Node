f5802113b11d1804360d5d15364f705f594337fc
ThanuraSilva <thanurasilva1989@gmail.com>
2025-04-27 10:14:22 +0530

    minor change-uuid function name

--- src/service/userService.js
+++ src/service/userService.js
@@ -7,7 +7,7 @@ async function addUser(user){
     try{
         const hashPassword = await bcrypt.hash(user.password, 10)
         const newUser = new User({
-            userId: uuid(),
+            userId: uuidv4(),
             firstName: user.firstName,
             lastName: user.lastName,
             email: user.email,
