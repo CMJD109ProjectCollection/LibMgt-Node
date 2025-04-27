a4efa08b4093e46d672f7655399a82d1b5eaaca1
ThanuraSilva <thanurasilva1989@gmail.com>
2025-04-27 10:06:15 +0530

    Create user service with bcrypt

---
+++ src/service/userService.js
@@ -0,0 +1,25 @@
+const User = require("../model/userModel")
+const  {v4: uuidv4} = require("uuid")
+const bcrypt = require("bcryptjs")
+
+async function addUser(user){
+
+    try{
+        const hashPassword = await bcrypt.hash(user.password, 10)
+        const newUser = new User({
+            userId: uuid(),
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            password: hashPassword
+            role: user.role
+         });
+         return newUser.save();
+    }catch(er){
+        console.error(er);
+        throw err;
+    }
+
+    module.exports = { addUser}
+
+}
